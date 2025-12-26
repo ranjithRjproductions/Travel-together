fa2100cbb20d13c4c1969f615afdaecc1ea1f717
ranjith S <ranjithkumarsrk123@gmail.com>
2025-12-26 19:51:35 +0000

    Your project Project name Firebase app studio-2888129045-980dc // Import

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-2888129045-980dc",
   "appId": "1:128915571060:web:fd563076251c920660ac21",
-  "storageBucket": "studio-2888129045-980dc.appspot.com",
+  "storageBucket": "studio-2888129045-980dc.firebasestorage.app",
   "apiKey": "AIzaSyAJVTvObBeFq3KoZw7IDr-yHQY6iEuRXW0",
   "authDomain": "studio-2888129045-980dc.firebaseapp.com",
   "measurementId": "",
