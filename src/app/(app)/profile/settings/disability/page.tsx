d6910a3d6f7d74bed02b9c4925ebc98e70426386
ranjith S <ranjithkumarsrk123@gmail.com>
2025-12-27 06:36:23 +0000

    ## Error Type Runtime ReferenceError
    ## Error Message
    Checkbox is not d

--- src/app/(app)/profile/settings/disability/page.tsx
+++ src/app/(app)/profile/settings/disability/page.tsx
@@ -13,6 +13,7 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Skeleton } from '@/components/ui/skeleton';
 import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
+import { Checkbox } from '@/components/ui/checkbox';
 
 const disabilitySchema = z.object({
   mainDisability: z.enum(['vision', 'hearing', 'none']).optional(),
