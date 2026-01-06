ed1e5476e9b83ee32bb19eb2f29f0433d1736c38
ranjith S <ranjithkumarsrk123@gmail.com>
2026-01-06 17:17:32 +0000

    ## Error Type Runtime Error
    ## Error Message
    useFirebase must be used w

--- src/components/app-layout.tsx
+++ src/components/app-layout.tsx
@@ -1,7 +1,10 @@
+'use client';
+
 import { AppLogo } from '@/components/app-logo';
 import { UserNav } from '@/components/user-nav';
 import { type User } from '@/lib/definitions';
 import { Footer } from './footer';
+import { FirebaseClientProvider } from '@/firebase';
 
 export default function AppLayout({
   children,
@@ -11,19 +14,21 @@ export default function AppLayout({
   user: User;
 }) {
   return (
-    <div className="flex min-h-screen w-full flex-col">
-      <header className="sticky top-0 z-50 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
-        <AppLogo />
-        <div className="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
-          <div className="ml-auto flex-1 sm:flex-initial">
-            <UserNav user={user} />
+    <FirebaseClientProvider>
+      <div className="flex min-h-screen w-full flex-col">
+        <header className="sticky top-0 z-50 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
+          <AppLogo />
+          <div className="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
+            <div className="ml-auto flex-1 sm:flex-initial">
+              <UserNav user={user} />
+            </div>
           </div>
-        </div>
-      </header>
-      <main id="main-content" className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
-        {children}
-      </main>
-      <Footer />
-    </div>
+        </header>
+        <main id="main-content" className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
+          {children}
+        </main>
+        <Footer />
+      </div>
+    </FirebaseClientProvider>
   );
 }
