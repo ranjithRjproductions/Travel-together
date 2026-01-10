934a8c3bcb52cfa6d5c34e07f27ab7876ae2a470
ranjith S <ranjithkumarsrk123@gmail.com>
2026-01-10 13:50:42 +0000

    does footor should come on login page when oped?

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,4 +1,5 @@
 import { AppLogo } from "@/components/app-logo";
+import { Footer } from "@/components/footer";
 
 export default function AuthLayout({
   children,
@@ -6,13 +7,19 @@ export default function AuthLayout({
   children: React.ReactNode;
 }) {
   return (
-    <main id="main-content" className="flex min-h-screen flex-col items-center justify-center p-4">
-       <div className="w-full max-w-sm">
-        <div className="mb-6">
-          <AppLogo />
+    <div className="flex flex-col min-h-screen">
+      <main
+        id="main-content"
+        className="flex-grow flex flex-col items-center justify-center p-4"
+      >
+        <div className="w-full max-w-sm">
+          <div className="mb-6">
+            <AppLogo />
+          </div>
+          {children}
         </div>
-        {children}
-      </div>
-    </main>
+      </main>
+      <Footer />
+    </div>
   );
 }
