7a22ea50b942e79fbcf915a4d68a44eed45b0a10
ranjith S <ranjithkumarsrk123@gmail.com>
2026-01-10 07:14:01 +0000

    ## Error Type Console Error
    ## Error Message
    A tree hydrated but some a

--- src/components/app-layout.tsx
+++ src/components/app-layout.tsx
@@ -1,11 +1,8 @@
 
-'use client';
-
 import { AppLogo } from '@/components/app-logo';
 import { UserNav } from '@/components/user-nav';
 import { type User } from '@/lib/definitions';
 import { Footer } from './footer';
-import { FirebaseClientProvider } from '@/firebase';
 
 export default function AppLayout({
   children,
@@ -25,21 +22,19 @@ export default function AppLayout({
   };
 
   return (
-    <FirebaseClientProvider>
-      <div className="flex min-h-screen w-full flex-col">
-        <header className="sticky top-0 z-50 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
-          <AppLogo homeUrl={getDashboardUrl()} />
-          <div className="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
-            <div className="ml-auto flex-1 sm:flex-initial">
-              <UserNav user={user} />
-            </div>
+    <div className="flex min-h-screen w-full flex-col">
+      <header className="sticky top-0 z-50 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
+        <AppLogo homeUrl={getDashboardUrl()} />
+        <div className="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
+          <div className="ml-auto flex-1 sm:flex-initial">
+            <UserNav user={user} />
           </div>
-        </header>
-        <main id="main-content" className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
-          {children}
-        </main>
-        <Footer />
-      </div>
-    </FirebaseClientProvider>
+        </div>
+      </header>
+      <main id="main-content" className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
+        {children}
+      </main>
+      <Footer />
+    </div>
   );
 }
